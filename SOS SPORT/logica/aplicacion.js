83def952b87801a26d7ba15c46a74ee7ad529770
JuanGarcia-hub <jgarciaroj@ucvvirtual.edu.pe>
2024-06-28 08:35:17 -0500

    Add files via upload

---
+++ SOS SPORT/logica/aplicacion.js
@@ -0,0 +1,31 @@
+function visibleFutbol(){
+    let tarjeta = document.getElementById('responsanbleFutbol');
+    tarjeta.style.display = "flex"
+}
+
+function responsableFutbol(){
+    let tarjeta = document.getElementById('responsanbleFutbol');
+    tarjeta.style.display = "none"
+}
+
+function responsableB(){
+    let tarjeta = document.getElementById('responsanbleB');
+    tarjeta.style.display = "none"
+}
+
+
+function visibleB(){
+    let tarjeta = document.getElementById('responsanbleB');
+    tarjeta.style.display = "flex"
+}
+
+function responsableE(){
+    let tarjeta = document.getElementById('responsanbleE');
+    tarjeta.style.display = "none"
+}
+
+
+function visibleE(){
+    let tarjeta = document.getElementById('responsanbleE');
+    tarjeta.style.display = "flex"
+}
